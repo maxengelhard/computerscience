77dfd84af6f93e274157022e3d9853ca1aa9f3fc
MaxVEngelhard <maxvengelhard@gmail.com>
2023-04-02 08:00:47 -0500

    Debug practice and compare

---
+++ compare.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 10;
+    int y = 20;
+
+    if (x < y ) {
+        printf("x is less than y\n");
+    }
+    else if (x > y ) {
+        printf("x is less not than y\n");
+    }
+    else {
+        printf("x is equal");
+    }
+}
